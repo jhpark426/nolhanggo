ad89b491d55b9e7f9f5257853c6651b35d0f5359
kcjo21 <cwh6272@naver.com>
2017-03-06 04:08:46 +0900

    AR 모듈 추가 및 버튼 구현
    AR버튼 으로 카메라 모드 실행 가능
    (서버 연동 X, 데이터값 전달 X)

---
+++ NH/NHARMODULE/src/main/java/com/szb/NHARMODULE/UnityPlayerNativeActivity.java
@@ -0,0 +1,16 @@
+package com.szb.NHARMODULE;
+
+import com.unity3d.player.*;
+import android.app.NativeActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+
+public class UnityPlayerNativeActivity extends UnityPlayerActivity
+{
+	@Override protected void onCreate (Bundle savedInstanceState)
+	{
+		Log.w("Unity", "UnityPlayerNativeActivity has been deprecated, please update your AndroidManifest to use UnityPlayerActivity instead");
+		super.onCreate(savedInstanceState);
+	}
+}
---
+++ NH/app/src/main/java/com/szb/nh/MainActivity.java
@@ -0,0 +1,25 @@
+package com.szb.nh;
+
+        import android.content.ComponentName;
+        import android.content.Intent;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View v){
+        Intent intent = new Intent();
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.setComponent(new ComponentName("com.szb.NHARMODULE", "com.szb.NHARMODULE.UnityPlayerNativeActivity"));
+        startActivity(intent);
+    }
+
+
+}
