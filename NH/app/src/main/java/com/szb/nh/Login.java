bcb3d2a53fe8d4b4139e568fe54019b80ed3c7e8
KwangHo-Shin <ssyyhh1024@gmail.com>
2017-03-07 15:47:38 +0900

    기본정리
    이거저거 만지다 다시 정리

--- NH/app/src/main/java/com/szb/nh/Login.java
+++ NH/app/src/main/java/com/szb/nh/Login.java
@@ -1,13 +1,29 @@
 package com.szb.nh;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+
+    Button btnSignin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnSignin = (Button)findViewById(R.id.LoginSignin);
+
+        btnSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), Home_Main.class);
+                startActivity(i);
+                finish();
+            }
+        });
     }
 }
--- NH/app/src/main/java/com/szb/nh/MainActivity.java
+++ NH/app/src/main/java/com/szb/nh/MainActivity.java
@@ -1,14 +1,39 @@
 package com.szb.nh;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnLogin, btnSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSignup = (Button)findViewById(R.id.Signup);
+        btnLogin = (Button)findViewById(R.id.Login);
+
+        btnSignup.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent i = new Intent(getApplication(), Signup.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(getApplicationContext(), Login.class));
+                finish();
+            }
+        });
     }
 
 }
