3642a8818065e88158072e68858b9adf5a8cb500
kcjo21 <cwh6272@naver.com>
2017-03-13 23:00:43 +0900

    유니티- 안드로이드 연동 완료
    모듈정리, 연동

--- NH/app/src/main/java/com/szb/nh/Home_Main.java
+++ NH/app/src/main/java/com/szb/nh/Home_Main.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class Home_Main extends AppCompatActivity {
 
@@ -17,7 +18,10 @@ public class Home_Main extends AppCompatActivity {
     public void onClick(View v){
         Intent intent = new Intent();
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        intent.setComponent(new ComponentName("com.szb.NHARMODULE", "com.szb.NHARMODULE.UnityPlayerNativeActivity"));
+        intent.setComponent(new ComponentName("com.szb.ARMODULE", "com.szb.ARMODULE.UnityPlayerNativeActivity"));
         startActivity(intent);
+    }
+    public void Touching(){
+        Toast.makeText(this, "Sc", Toast.LENGTH_SHORT).show();
     }
 }
