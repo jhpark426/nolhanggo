ea2ec6f41d3b508f8a3d6df104feb29c3de0a469
kcjo21 <cwh6272@naver.com>
2017-04-12 04:00:23 +0900

    프레그먼트, 타이틀바, 탭바

--- NH/ARMODULE/src/main/java/com/szb/ARMODULE/Home/Home_Main.java
+++ NH/ARMODULE/src/main/java/com/szb/ARMODULE/Home/Home_Main.java
@@ -115,9 +115,9 @@ public class Home_Main extends AppCompatActivity {
         public android.support.v4.app.Fragment getItem(int position) {
             switch (position) {
                 case 0:
-                    return new Frag_Quiz();
-                case 1:
                     return new Frag_Home();
+                case 1:
+                    return new Frag_Quiz();
                 case 2:
                     return new Frag_Info();
                 case 3:
