017791b5ff6b8d3b5578362d5b62e02d4549433b
kcjo21 <cwh6272@naver.com>
2017-04-11 18:05:09 +0900

    뷰페이저

--- NH/ARMODULE/src/main/java/com/szb/ARMODULE/Home/Home_Main.java
+++ NH/ARMODULE/src/main/java/com/szb/ARMODULE/Home/Home_Main.java
@@ -2,12 +2,12 @@ package com.szb.ARMODULE.Home;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageView;
 
 import com.szb.ARMODULE.GameActivity;
 import com.szb.ARMODULE.R;
@@ -16,9 +16,7 @@ import com.szb.ARMODULE.R;
 public class Home_Main extends AppCompatActivity {
 
     ViewPager viewpager;
-
-
-
+    ConstraintLayout top;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,15 +24,63 @@ public class Home_Main extends AppCompatActivity {
         setContentView(R.layout.activity_home_main);
 
         viewpager = (ViewPager)findViewById(R.id.viewpager);
+        top = (ConstraintLayout) findViewById(R.id.top);
 
 
-        Button bt_menu_open = (Button)findViewById(R.id.bt_menu_open);
+
+        ImageView bt_home = (ImageView)findViewById(R.id.bt_home);
+        ImageView bt_quiz = (ImageView)findViewById(R.id.bt_quiz);
+        ImageView bt_info = (ImageView)findViewById(R.id.bt_info);
+        ImageView bt_rank = (ImageView)findViewById(R.id.bt_rank);
+
         viewpager.setAdapter(new pagerAdapter(getSupportFragmentManager()));
-        viewpager.setCurrentItem(1);
+        viewpager.setCurrentItem(0);
 
-        bt_menu_open.setOnClickListener(movePageListener);
-        bt_menu_open.setTag(0);
+        bt_home.setOnClickListener(movePageListener);
+        bt_home.setTag(0);
+        bt_quiz.setOnClickListener(movePageListener);
+        bt_quiz.setTag(1);
+        bt_info.setOnClickListener(movePageListener);
+        bt_info.setTag(2);
+        bt_rank.setOnClickListener(movePageListener);
+        bt_rank.setTag(3);
+
+        bt_home.setSelected(true);
+
+        viewpager.addOnPageChangeListener(new ViewPager.OnPageChangeListener()
+        {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels)
+            {
+            }
+            @Override
+            public void onPageSelected(int position)
+            {
+                int i = 0;
+                while(i<4)
+                {
+                    if(position==i)
+                    {
+                        top.findViewWithTag(i).setSelected(true);
+                    }
+                    else
+                    {
+                        top.findViewWithTag(i).setSelected(false);
+                    }
+                    i++;
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state)
+            {
+
+            }
+        });
+
     }
+
+
 
     View.OnClickListener movePageListener = new View.OnClickListener()
     {
@@ -42,6 +88,20 @@ public class Home_Main extends AppCompatActivity {
         public void onClick(View v)
         {
             int tag = (int) v.getTag();
+            int i = 0;
+            while(i<4)
+            {
+                if(tag==i)
+                {
+                    top.findViewWithTag(i).setSelected(true);
+                }
+                else
+                {
+                    top.findViewWithTag(i).setSelected(false);
+                }
+                i++;
+            }
+
             viewpager.setCurrentItem(tag);
         }
     };
@@ -55,17 +115,22 @@ public class Home_Main extends AppCompatActivity {
         public android.support.v4.app.Fragment getItem(int position) {
             switch (position) {
                 case 0:
-                    return new MenuFragment2();
+                    return new Frag_Quiz();
                 case 1:
-                    return new MenuFragment();
+                    return new Frag_Home();
+                case 2:
+                    return new Frag_Info();
+                case 3:
+                    return new Frag_Rank();
                 default:
                     return null;
             }
         }
 
         @Override
-        public int getCount() {
-            return 2;
+        public int getCount()
+        {
+            return 4;
         }
 
     }
